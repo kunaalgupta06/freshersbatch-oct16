cb17f02980e94283818def296220391e2a2f2c01
Kunal Gupta <kunaalgupta1920@gmail.com>
2021-09-30 11:56:36 +0530

    update js files

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 console.log('hello');
-hhj
+function myapp()
--- myScript.js
+++ myScript.js
@@ -1,3 +1,4 @@
 function myfunction() {
             document.getElementById("abc").innerHTML="Paragraph changed";
         }
+console.log('js-assignmens')
